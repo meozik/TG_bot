aa321e5811335b27f4c44757000e8005ca5cde65
Meoz <m.knysh@fexnet.com>
2018-10-17 16:42:22 +0300

    Добавил методы работы с БД, метод сравнения обьема

--- Binance_api_call.py
+++ Binance_api_call.py
@@ -1,20 +1,20 @@
-import psycopg2 as psycopg2
 import requests
 import psycopg2
-import sys
-import sqlite3
-import json
+import time
 
 
-def parse_price_json():
+def get_binance_json():
     url = 'https://api.binance.com'
-    user ='meozik'
+    user = 'meozik'
     password = 'meozds9205'
-    r = requests.get(url+'/api/v1/ticker/24hr')
+    r = requests.get(url + '/api/v1/ticker/24hr')
     return r.json()
+
+
 def parse_wallet_state():
     url = 'https://api.binance.com'
-    r = requests.get(url + '/api/v3/account',)
+    r = requests.get(url + '/api/v3/account', )
+
 
 def db_connect():
     conn_string = "host='localhost' dbname='binance_bot_db' user='postgres' password='meozds9205'"
@@ -23,7 +23,8 @@ def db_connect():
     cursor = conn.cursor()
     print('Connected!\n')
 
-def db_save(pair_name,price,pricechangepercent,pricechangeabsolut,volume):
+
+def db_save(pair_name, price, pricechangepercent, pricechangeabsolut, volume):
     # Коннектимся к локальной базе данных
     conn_string = "host='localhost' dbname='binance_bot_db' user='postgres' password='meozds9205'"
     print("Connecting to database\n	->%s" % (conn_string))
@@ -31,33 +32,112 @@ def db_save(pair_name,price,pricechangepercent,pricechangeabsolut,volume):
     cursor = conn.cursor()
     print('Connected!\n')
     # Вставляем данные в таблицу
-    cursor.execute("""INSERT INTO binance_api_response(pair_name, price, pricechangepercent, pricechangeabsolut, volume)
-                          VALUES ('{0}', '{1}', '{2}', '{3}', '{4}')""".format(pair_name, price, pricechangepercent, pricechangeabsolut, volume)
+    cursor.execute(f"""INSERT INTO binance_api_response(pair_name, price, pricechangepercent, pricechangeabsolut, volume)
+                      VALUES ('{pair_name}', '{price}', '{pricechangepercent}', '{pricechangeabsolut}', '{volume}')"""
                    )
     # Сохраняем изменения
     conn.commit()
+    print("Запись произведена успешно!")
+    cursor.close()
+    del cursor
+    conn.close()
 
 
-db_save(parse_price_json()[0]['symbol'],float('0.2'),float('0.213'),float('1.321'),float('1.312321'))
+def db_update(price, pricechangepercent, pricechangeabsolut, volume):
+    # Коннектимся к локальной базе данных
+    conn_string = "host='localhost' dbname='binance_bot_db' user='postgres' password='meozds9205'"
+    print("Connecting to database\n	->%s" % (conn_string))
+    conn = psycopg2.connect(conn_string)
+    cursor = conn.cursor()
+    print('Connected!\n')
+    cursor.execute(
+        f"""UPDATE binance_api_response SET price = {price}, pricechangepercent = {pricechangepercent}, pricechangeabsolut = {pricechangeabsolut}, volume = {volume} where id=1""")
+    # Сохраняем изменения
+    conn.commit()
+    print("Запись произведена успешно!")
+    cursor.close()
+    del cursor
+    conn.close()
+
+def get_old_volume(i):
+    # Коннектимся к локальной базе данных
+    conn_string = "host='localhost' dbname='binance_bot_db' user='postgres' password='meozds9205'"
+    print("Connecting to database\n	->%s" % (conn_string))
+    conn = psycopg2.connect(conn_string)
+    cursor = conn.cursor()
+    print('Connected!\n')
+    # Выбираем старые данные по айдишнику валюты
+    num = i+1
+    cursor.execute(
+        f'SELECT volume FROM binance_api_response WHERE id= {num}')
+    # Парсим данные в переменную
+    result = cursor.fetchall()[i][0]
+    cursor.close()
+    del cursor
+    conn.close()
+    return result
+
+def get_price(i):
+    avgPrice = (float(get_binance_json()[i]['bidPrice'])+float(get_binance_json()[i]['askPrice']))/2
+    return float(avgPrice)
+
+def get_old_price(i):
+    # Коннектимся к локальной базе данных
+    conn_string = "host='localhost' dbname='binance_bot_db' user='postgres' password='meozds9205'"
+    print("Connecting to database\n	->%s" % (conn_string))
+    conn = psycopg2.connect(conn_string)
+    cursor = conn.cursor()
+    print('Connected!\n')
+    # Выбираем старые данные по айдишнику валюты
+    cursor.execute(
+        """SELECT price FROM binance_api_response WHERE id={} """.format(int(i)+1))
+    # Парсим данные в переменную
+    result = cursor.fetchall()[int(i)][0]
+    cursor.close()
+    del cursor
+    conn.close()
+    return result
+
+def compare_volume():
+    # забираем данные предыдущего значения объема торгов
+    old = float(get_old_volume(0))
+    print('Старое значение объема', old)
+    # Забираем данные нового объема торгов
+    new = float(get_binance_json()[0]['quoteVolume'])
+    print('Новое значение объема', new)
+    # Сравниваем данные
+    diff = ((new - old)/old)*100
+    if  diff>= 10:
+        print('Алярм!!!!')
+    print('Разница в процентах',diff)
+
+def compare_price():
+    # забираем данные предыдущего значения объема торгов
+    old = float(get_old_volume(0))
+    print('Старое значение объема', old)
+    # Забираем данные нового объема торгов
+    new = float(get_binance_json()[0]['quoteVolume'])
+    print('Новое значение объема', new)
+    # Сравниваем данные
+    diff = ((new - old)/old)*100
+    if  diff>= 10:
+        print('Алярм!!!!')
+    print('Разница в процентах',diff)
 
 
-# print(parse_price_json()[0]['symbol'])
-print(parse_price_json()[0])
+# compare_volume()
+# print(get_binance_json()[0])
+print(type(get_old_volume(0)))
 
 
-#
-# for i in range(len(parse_price_json())):
-#     if parse_price_json()[i]['symbol'] == 'TRXBTC':
-#         print(i)
-
-# print(len(parse_price_json()))
-# pairName = parse_price_json()[0]['symbol']
-# volume = float(parse_price_json()[0]['volume'])
-# priceChange = float(parse_price_json()[0]['priceChange'])
-# print('Pair name =', pairName, 'Volume =', volume, 'Price change', priceChange)
-
-# for i in range(len(parse_price_json())-1):
-#     print(parse_price_json()[i])
-#     print('Pair name =', pairName, 'Volume =', volume, 'Price change', priceChange)
 
 
+
+# while True:
+#     i = 0
+#     print(get_binance_json()[i])
+#     # db_save(parse_price_json()[0]['symbol'], float('0.0'), float(parse_price_json()[0]['priceChangePercent']),
+#     #         float(parse_price_json()[0]['priceChange']), float(parse_price_json()[0]['quoteVolume']))
+#     db_update(get_price(i), float(get_binance_json()[i]['priceChangePercent']),
+#               float(get_binance_json()[i]['priceChange']), float(get_binance_json()[i]['quoteVolume']))
+#     time.sleep(1)
